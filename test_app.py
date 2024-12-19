0afc64f57aa815f6aa5accfd37162bb87252272e
Manuelgujo <guglio1996@gmail.com>
2024-12-19 17:19:09 +0100

    style: format code with Black [again]

--- test_app.py
+++ test_app.py
@@ -6,13 +6,14 @@ from app import app
 def test_version():
     """Test the version endpoint using Flask test client."""
     with app.test_client() as client:
-        response = client.get('/version')
+        response = client.get("/version")
         assert response.status_code == 200
         assert response.json == {"version": "v0.0.1"}
+
 
 def test_temperature():
     """Test the temperature endpoint using Flask test client."""
     with app.test_client() as client:
-        response = client.get('/temperature')
+        response = client.get("/temperature")
         assert response.status_code == 200
         assert "average_temperature" in response.json
